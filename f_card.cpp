27259bcf16fd55e1ef3dbfd2e1c36f781e8f4af1
Rohit Sharma <116941210+Rohitsharma1905@users.noreply.github.com>
2023-10-02 18:41:09 +0530

    Create f_card.cpp

---
+++ f_card.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+
+        // Calculate the minimum number of operations
+        int min_operations = min(x, n - x);
+
+        cout << min_operations << endl;
+    }
+
+    return 0;
+}
