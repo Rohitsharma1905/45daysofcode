a9df07b679b73397094518d68f4e32a927a9d88c
Rohit Sharma <116941210+Rohitsharma1905@users.noreply.github.com>
2023-10-26 23:45:50 +0530

    Create trade_surplus.cpp

---
+++ trade_surplus.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while(T--) {
+        int A1, A2, B1, B2;
+        cin >> A1 >> A2 >> B1 >> B2;
+        if(A1 + B1 > A2 + B2) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
