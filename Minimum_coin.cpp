086b916f39023756af073b0e6e1df9db0a5637ee
Rohit Sharma <116941210+Rohitsharma1905@users.noreply.github.com>
2023-10-27 11:25:50 +0530

    Create Minimum_coin.cpp

---
+++ Minimum_coin.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while(T--) {
+        int X;
+        cin >> X;
+        if(X % 5 != 0) {
+            cout << -1 << endl;
+        } else {
+            cout << X / 10 + (X % 10) / 5 << endl;
+        }
+    }
+    return 0;
+}
