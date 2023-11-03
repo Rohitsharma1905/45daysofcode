6cb0d86601c1b7d46d9f51dfc517767f47e4924f
Rohit Sharma <116941210+Rohitsharma1905@users.noreply.github.com>
2023-11-03 21:32:21 +0530

    Create sasta_shark.cpp

---
+++ sasta_shark.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b;
+        cin>>a>>b;
+        if(a*10==b*5)
+            cout<<"ANY\n";
+        else if(a*10>b*5)
+            cout<<"FIRST\n";
+        else
+            cout<<"SECOND\n";
+    }
+    return 0;
+}
